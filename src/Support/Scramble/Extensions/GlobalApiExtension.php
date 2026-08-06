9f1eba62efd27d3852d2579f90dfdccd3bb0b3b4
Maksim Tsatsura <maksim.tsatsura>
2026-08-06 16:03:00 +0300

    fix: GlobalApiExtension

--- src/Support/Scramble/Extensions/GlobalApiExtension.php
+++ src/Support/Scramble/Extensions/GlobalApiExtension.php
@@ -4,20 +4,21 @@ declare(strict_types=1);
 
 namespace Nicole\Box\Core\Support\Scramble\Extensions;
 
-use Dedoc\Scramble\Contracts\OperationTransformer;
+use Dedoc\Scramble\Extensions\OperationExtension;
 use Dedoc\Scramble\Support\Generator\Operation;
 use Dedoc\Scramble\Support\Generator\Parameter;
 use Dedoc\Scramble\Support\Generator\Schema;
 use Dedoc\Scramble\Support\Generator\Types\StringType;
 use Dedoc\Scramble\Support\RouteInfo;
 
-class GlobalApiExtension implements OperationTransformer
+class GlobalApiExtension extends OperationExtension
 {
-  public function transform(Operation $operation, RouteInfo $routeInfo): void
+  public function handle(Operation $operation, RouteInfo $routeInfo): void
   {
+
     $uri = ltrim($routeInfo->route->uri(), '/');
 
-    if (str_starts_with($uri, 'api/')) {
+    if (str_starts_with($uri, 'api/v1') || str_starts_with($uri, 'api/')) {
       $operation->addParameters([
         Parameter::make('X-Sales-Channel', 'header')
           ->description('Код канала продаж для контекста настроек (например, widget или catalog).')
